be7c5c7be2a440ece28f3920159546b92b4a2d91
Tirsitt <tirsitbele@gmail.com>
2025-02-15 01:06:43 +0300

    Initial commit

---
+++ StoreApp/Migrations/20250214140215_new.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class @new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "54a27755-d202-41ee-b01f-c342a335972e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a403b016-faf1-4eba-998a-d0badceddae2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cab0988a-fbba-4d9e-82cd-3c81e7fbacdf");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "248c1ba4-17f9-4d75-8659-e7474cdaa334", null, "Editor", "EDITOR" },
+                    { "5a6e566e-570f-460d-bf9b-974df1c0bee2", null, "User", "USER" },
+                    { "96f5c59d-31e7-4a68-87f1-bd2af51b157e", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1,
+                column: "CategoryName",
+                value: "Books");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2,
+                column: "CategoryName",
+                value: "Electronics");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6,
+                column: "Price",
+                value: 125m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7,
+                column: "Price",
+                value: 145m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9,
+                column: "Price",
+                value: 445m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 12,
+                column: "Price",
+                value: 345m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 15,
+                column: "Price",
+                value: 199m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "248c1ba4-17f9-4d75-8659-e7474cdaa334");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5a6e566e-570f-460d-bf9b-974df1c0bee2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "96f5c59d-31e7-4a68-87f1-bd2af51b157e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "54a27755-d202-41ee-b01f-c342a335972e", null, "Editor", "EDITOR" },
+                    { "a403b016-faf1-4eba-998a-d0badceddae2", null, "Admin", "ADMIN" },
+                    { "cab0988a-fbba-4d9e-82cd-3c81e7fbacdf", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1,
+                column: "CategoryName",
+                value: "Book");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2,
+                column: "CategoryName",
+                value: "Electronic");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6,
+                column: "Price",
+                value: 25m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7,
+                column: "Price",
+                value: 45m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9,
+                column: "Price",
+                value: 4445m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 12,
+                column: "Price",
+                value: 845m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 15,
+                column: "Price",
+                value: 99m);
+        }
+    }
+}
